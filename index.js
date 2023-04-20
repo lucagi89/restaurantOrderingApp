12db2239010e14054187eff43c30d6cbc4164a62
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-04-20 10:35:31 +0100

    general trying

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import { menuArray } from "/data.js"
 const itemsList = document.getElementById('items-list')
 const cart = document.getElementById('cart')
 const paymentForm = document.getElementById('pay-form')
-let cartArray = []
+let itemsArray = []
+let itemCounts = {}
 
 
 document.addEventListener('click', function(e){
@@ -30,14 +31,26 @@ document.addEventListener('click', function(e){
 function addItem(idItem){
     const itemObj = menuArray.filter(function(item){
         return item.id == idItem})[0]
-    cartArray.push(itemObj)
-    renderCart()
+    itemsArray.push(itemObj)
+    itemsCountFunction(itemObj)
     }
 
+function itemsCountFunction(obj){
+    const arrayNames = []
+    itemsArray.forEach(item => { arrayNames.push(item.name)})
+    itemCounts = arrayNames.filter(item => {return obj.name === item}).length
+    let itemCountsObj = {name: obj.name, value: itemCounts}    
+    
+    console.log(itemCountsObj)
+    renderCart(itemCountsObj)
+}
+
+
+
 function removeItem(item){
-    cartArray.splice(item, 1)
+    itemsArray.splice(item, 1)
     renderCart()
-    if(cartArray.length == 0){
+    if(itemsArray.length == 0){
         cart.classList.add('hidden')
     }
 }
@@ -54,6 +67,7 @@ function orderComplete(){
 }
 
 
+
 function hide(div){
     if(!div.classList.contains('hidden')){
         div.classList.add('hidden')
@@ -67,28 +81,36 @@ function show(div){
     }
 }
 
-function renderCart(){
+function renderCart(name, value){
     const cartItemContainer = document.getElementById('cart-item-container')
     const price = document.getElementById('price')
+    let letterS = ''
     let totalPrice = 0
     let cartItemsHtml = ''
-    if (cartArray){
-     cartArray.forEach(function(cartItem, index){
-        totalPrice += cartItem.price
+
+    if(value>1){
+        letterS = 's'
+    }
+    else{
+        letterS = ''
+    }
+        let itemObj = itemsArray.filter(function(item){ 
+            return item.name === name})[0]
+        totalPrice += itemObj.price
         cartItemsHtml += `
         <div id = "cart-item" class = "cart-item">
-            <h2 id="item-name" class="item-name">${cartItem.name}</h2>
-            <button id="remove-btn" class="remove-btn" data-removebtn="${index}">Remove</button>
-            <h3 id='item-price'>$${cartItem.price}</h3>
+            <h2 id="item-name" class="item-name">${itemObj.value} ${itemObj.name}${letterS}</h2>
+            <button id="remove-btn" class="remove-btn" data-removebtn="x">Remove</button>
+            <h3 id='item-price'>$${itemObj.price*cartItem.value}</h3>
         </div>
         `
-    })
+    }
     price.textContent = `$${totalPrice}`
     cartItemContainer.innerHTML = cartItemsHtml
     show(cart)
 
 }
-}
+
 
 
 
